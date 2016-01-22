1a20c0ea7924ae3c172abd15421976ac41ed9c99
Bill Curtis <wcurtis@sweettoothhq.com>
2016-01-21 20:48:34 -0500

    Add auto update code

--- main.js
+++ main.js
@@ -6,27 +6,42 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+const updater = require('electron-updater');
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  updater.on('ready', function () {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
   });
+
+  updater.on('updateRequired', function () {        
+      app.quit();
+  });
+
+  updater.on('updateAvailable', function () {
+      mainWindow.webContents.send('update-available');
+  });
+
+  updater.start();
 }
 
 // This method will be called when Electron has finished
